1d5f20bd105a69e648f660e1afda9416a7fbed20
Kochetov <kochetov.yevhen.dev@gmail.com>
2024-01-20 17:54:17 +0200

    add startModal and loginModal in uiStuff file

--- public/uiStuff.js
+++ public/uiStuff.js
@@ -1,1 +1,29 @@
-console.log('HEllo')
+// Set height and width of canvas = window
+
+let wHeight = window.innerHeight;
+let wWidth = window.innerWidth;
+
+const canvas = document.querySelector("#the-canvas");
+const context = canvas.getContext("2d");
+
+canvas.height = wHeight;
+canvas.width = wWidth;
+const player = {}; // This will be all things "This" player
+
+// put the modals into variables so we can interact with them
+const loginModal = new bootstrap.Modal(document.querySelector("#loginModal"));
+const spawnModal = new bootstrap.Modal(document.querySelector("#spawnModal"));
+
+window.addEventListener("load", () => {
+  // on page load, open the login modal
+  loginModal.show();
+});
+
+document.querySelector(".name-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  // console.log('Submitted')
+  player.name = document.querySelector("#name-input").value;
+  loginModal.hide();
+  spawnModal.show();
+  console.log(player);
+});
